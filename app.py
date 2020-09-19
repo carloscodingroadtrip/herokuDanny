114f53add379a8071aa8e51713a0976ebf566320
Carlos Molina <molina.carlos.dev@gmail.com>
2020-09-18 23:00:57 -0500

    Added imports, debug and api route

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
-from flask import Flask, jsonify, render_template
-
+from flask import (Flask, render_template, jsonify, request, redirect)
+import pull
+import processInputs
+from werkzeug.http import HTTP_STATUS_CODES
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 app.static_folder = 'static'
 
-
 #################################################
 # Flask Routes
 #################################################
 
+
 @app.route("/")
 def welcome():
     return render_template("index.html")
+
+
+@app.route('/apis')
+def apis():
+    return(
+        f'AVAILABLE API ROUTES: <br>'
+        f'_______________________________  <br>'
+        f'<br>ALL BABY NAMES....:  /api/babynames<br>'
+        f'<br>'
+
+        f'BUDGET FILTER ONLY JSON:  /api/budget'
+    )
+
+##########  ERROR HANDLING ##################
+
+
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
+
+
+############# FLASK CLOSING CODE ###################
+if __name__ == '__main__':
+    app.run(debug=True)
