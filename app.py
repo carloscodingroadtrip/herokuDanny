9cd92d982e631a09732af844bff6a18c0c66f9fe
Carlos Molina <molina.carlos.dev@gmail.com>
2020-09-18 22:09:03 -0500

    Updating return HTML

--- app.py
+++ app.py
@@ -13,10 +13,5 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return (
-        f"<h4>Available Routes:</h4>"
-        f'<a href="/api/v1.0/ids">/api/v1.0/ids</a><br/>'
-        f'<a href="/api/v1.0/info/1286">/api/v1.0/info/subject_id</a><br/>'
-        f'<a href="/api/v1.0/subjects">/api/v1.0/subjects</a><br/>'
-        f'<a href="/api/v1.0/subjects/1286">/api/v1.0/subjects/subject_id</a><br/>'
-        f'<a href="/"><h4>Back</h4></a><br/>'
+        f"<h4>DANNY ROUTES.... :</h4>"
     )
