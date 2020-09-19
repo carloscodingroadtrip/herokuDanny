c57b765df43da431901e7d916adeaa2476d93448
Carlos Molina <molina.carlos.dev@gmail.com>
2020-09-19 13:59:33 -0500

    added to app py the urlfor from flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import (Flask, render_template, jsonify, request, redirect)
+from flask import (Flask, render_template, jsonify, request, url_for, redirect)
 from werkzeug.http import HTTP_STATUS_CODES
 
 #################################################
