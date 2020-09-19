e65afef036aa86760796e453393ee9e7a0e923ce
Carlos Molina <molina.carlos.dev@gmail.com>
2020-09-18 22:54:31 -0500

    Added static to my server

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, render_template
 # Flask Setup
 #################################################
 app = Flask(__name__)
+app.static_folder = 'static'
 
 
 #################################################
